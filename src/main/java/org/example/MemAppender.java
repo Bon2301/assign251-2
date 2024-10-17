15b4415edac5289dd6871a80db9fe4a672c9ad24
Ben <bjmullinger@gmail.com>
2024-10-17 15:16:21 +1300

    adding test finished, working on LogTest

--- src/main/java/org/example/MemAppender.java
+++ src/main/java/org/example/MemAppender.java
@@ -1,12 +1,9 @@
 package org.example;
 
+import jdk.jshell.spi.ExecutionControlProvider;
 import org.apache.log4j.*;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.text.DateFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
@@ -22,13 +19,13 @@ public class MemAppender {
     private static Logger LOG;
     private static List<String> logList = new ArrayList<String>();
     private static int maxSize;
+    private static Appender append;
     private static String pattern;
     private static Layout layout;
-    private static Appender append;
     private static int curSize;
     private static long discLogs;
 
-    private MemAppender(Logger LOG, String pattern, Layout layout, Appender append){
+    private MemAppender(){
         super();
         BasicConfigurator.configure();
         LOG = Logger.getLogger("Log");
@@ -36,64 +33,68 @@ public class MemAppender {
         maxSize = 2;
         curSize = 0;
         discLogs = 0;
-        append = (Appender)LOG.getAllAppenders().nextElement();
+        append = LOG.getAppender("appender");
         pattern = "%p: %d{dd/MM/yyyy} in %t - %m%n";
         layout = new org.apache.log4j.PatternLayout(pattern);
-        append.setLayout(layout);
-        for(int i=0; i<maxSize; i++){
-            logList.add("testString");
-        }
-        System.out.println(logList);
+        LOG.addAppender(append);
+    }
 
-    }
-    private MemAppender() {}
 
-    private static MemAppender instance = new MemAppender();
+        private static MemAppender instance = new MemAppender();
 
-    public static MemAppender getInstance(){
-        return instance;
-    }
-    public void check(){
-        System.out.println("object successfully created");
-    }
-    public void addLog(String x){
-//        curSize++;
-//        if(curSize>maxSize) {
-//            logList.removeFirst();
-//            logList.add(x);
-//        } else{
-//            logList.add(x);
-//        }
-        logList.removeFirst();
-        logList.add(x);
-    }
-    public void getCurrentLogs(){
-        for(int i=0; i<logList.size(); i++){
-            LOG.info(logList.get(i));
+        public static MemAppender getInstance () {
+            return instance;
         }
-    }
-    public void getEventStrings(){
-        for(int i=0; i<logList.size(); i++){
-            System.out.println(logList.get(i));
+        public void check () {
+            System.out.println("object successfully created");
         }
-    }
-    public void printLogs(){
-        for(int i=0; i<logList.size(); i++){
-            LOG.info(logList.get(i));
+        public void addLog (String x){
+            curSize++;
+            if (curSize > maxSize) {
+                logList.removeFirst();
+                logList.add(x);
+            } else {
+                logList.add(x);
+            }
         }
-        discLogs += logList.size();
-        logList.clear();
-    }
-    public void getDiscardedLogCount(){
-        System.out.println("Total discarded logs:");
-        System.out.println(discLogs);
-    }
-    public String getCurrentList(int i){
-        return logList.get(i);
-    }
-    public int getMaxSize(){
-        return maxSize;
-    }
+        public void discardLog(int i){
+            curSize--;
+            logList.remove(i);
+        }
+
+        public void getCurrentLogs () {
+            for (int i = 0; i < logList.size(); i++) {
+                LOG.info(logList.get(i));
+            }
+        }
+        public void getEventStrings () {
+            for (int i = 0; i < logList.size(); i++) {
+                System.out.println(logList.get(i));
+            }
+        }
+        public void printLogs () {
+            for (int i = 0; i < logList.size(); i++) {
+                LOG.info(logList.get(i));
+            }
+            discLogs += logList.size();
+            logList.clear();
+        }
+        public void getDiscardedLogCount () {
+            System.out.println("Total discarded logs:");
+            System.out.println(discLogs);
+        }
+        public String getCurrentList ( int i){
+            return logList.get(i);
+        }
+        public int getMaxSize() {
+            return maxSize;
+        }
+        public String getLogAsString(int i){
+            StringWriter sw = new StringWriter();
+            WriterAppender  wa = new WriterAppender(layout, sw);
+            LOG.addAppender(wa);
+            return sw.toString();
+        }
 
 
 }
--- src/test/java/org.example/Test3.java
+++ src/test/java/org.example/Test3.java
@@ -2,15 +2,28 @@ package org.example;
 
 
 import org.junit.Test;
+
+import java.io.CharArrayWriter;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class Test3 {
     MemAppender ma = MemAppender.getInstance();
 
     @Test
     public void addingTest() throws Exception{
-        ma.addLog("This is Log 1");
-        assertEquals(ma.getCurrentList(0),"This is Log 1");
+        String s = "testing string";
+        ma.addLog(s);
+        assertEquals("testing string", ma.getCurrentList(0));
+        ma.discardLog(0);
+    }
+    @Test
+    public void currentLogTest() throws Exception{
+        String s = "testing log";
+        ma.addLog(s);
+        //assertEquals("testing log", ma.getCurrentList(0));
+         assertEquals("INFO: 17/10/2024 in main - testing log", ma.getLogAsString(0));
     }
 
 }
