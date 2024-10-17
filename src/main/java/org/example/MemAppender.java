2c1122f512ff5adfa32d6175ce5203fc9c7a9245
Ben <bjmullinger@gmail.com>
2024-10-17 15:49:44 +1300

    log test  and layout test working

--- src/main/java/org/example/MemAppender.java
+++ src/main/java/org/example/MemAppender.java
@@ -18,7 +18,7 @@ import org.apache.log4j.Logger;
 public class MemAppender {
     private static Logger LOG;
     private static List<String> logList = new ArrayList<String>();
-    private static int maxSize;
+    private static long maxSize;
     private static Appender append;
     private static String pattern;
     private static Layout layout;
@@ -27,27 +27,38 @@ public class MemAppender {
 
     private MemAppender(){
         super();
+        //setup of the Logger
         BasicConfigurator.configure();
         LOG = Logger.getLogger("Log");
         LOG.setLevel(Level.ALL);
+        //the maxSize variable can be changed to suit however many the user wants
         maxSize = 2;
         curSize = 0;
         discLogs = 0;
+        //setup of the appender and the layout
         append = LOG.getAppender("appender");
-        pattern = "%p: %d{dd/MM/yyyy} in %t - %m%n";
+        pattern = "%p - %d{dd MMM yyyy} in %t - %m";
         layout = new org.apache.log4j.PatternLayout(pattern);
         LOG.addAppender(append);
     }
 
-
+        //setup of the singleton pattern
         private static MemAppender instance = new MemAppender();
-
         public static MemAppender getInstance () {
             return instance;
         }
         public void check () {
             System.out.println("object successfully created");
         }
+        public void setLayout(String patternToBeUsed){
+            LOG.removeAppender(append);
+            append = LOG.getAppender("appender");
+            pattern = patternToBeUsed;
+            layout = new org.apache.log4j.PatternLayout(pattern);
+            LOG.addAppender(append);
+        }
+
+        //this function is used to add strings into the memory list ready to be logged
         public void addLog (String x){
             curSize++;
             if (curSize > maxSize) {
@@ -57,21 +68,25 @@ public class MemAppender {
                 logList.add(x);
             }
         }
+        //this is used to discard logs from the memory list
         public void discardLog(int i){
             curSize--;
             logList.remove(i);
+            discLogs++;
         }
-
+        //this prints the current logs held in memory into the console
         public void getCurrentLogs () {
             for (int i = 0; i < logList.size(); i++) {
                 LOG.info(logList.get(i));
             }
         }
+        //thi prints the current list of strings held in memory into the console
         public void getEventStrings () {
             for (int i = 0; i < logList.size(); i++) {
                 System.out.println(logList.get(i));
             }
         }
+        //this prints the logs held in memory and then removes them from the memory
         public void printLogs () {
             for (int i = 0; i < logList.size(); i++) {
                 LOG.info(logList.get(i));
@@ -79,21 +94,30 @@ public class MemAppender {
             discLogs += logList.size();
             logList.clear();
         }
+        //this prints the current amount of logs discarded from memory
         public void getDiscardedLogCount () {
             System.out.println("Total discarded logs:");
             System.out.println(discLogs);
         }
+        //this is used in the tests to assert the string that was inputted
         public String getCurrentList ( int i){
             return logList.get(i);
         }
-        public int getMaxSize() {
+
+        //this is used to get the max size that the list can be
+        public long getMaxSize() {
             return maxSize;
         }
+
         public String getLogAsString(int i){
             StringWriter sw = new StringWriter();
             WriterAppender  wa = new WriterAppender(layout, sw);
             LOG.addAppender(wa);
-            return sw.toString();
+            LOG.info(logList.get(i));
+            String s = sw.toString();
+            LOG.removeAppender(wa);
+            return s;
+
         }
 
 
--- src/test/java/org.example/Test3.java
+++ src/test/java/org.example/Test3.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.assertThat;
 public class Test3 {
     MemAppender ma = MemAppender.getInstance();
 
+    //This test is used to see if the inputted string is being added to the list of logs
     @Test
     public void addingTest() throws Exception{
         String s = "testing string";
@@ -18,12 +19,31 @@ public class Test3 {
         assertEquals("testing string", ma.getCurrentList(0));
         ma.discardLog(0);
     }
+    //This test is used to see if the inputted string can be printed as a log
     @Test
     public void currentLogTest() throws Exception{
         String s = "testing log";
         ma.addLog(s);
-        //assertEquals("testing log", ma.getCurrentList(0));
-         assertEquals("INFO: 17/10/2024 in main - testing log", ma.getLogAsString(0));
+        assertEquals("INFO - 17 Oct 2024 in main - testing log", ma.getLogAsString(0));
+        ma.discardLog(0);
     }
+    //This test is used to check if the layout can be changed
+    @Test
+    public void setLayoutTest() throws Exception{
+        String pattern = "%m - %t - %d{dd MM yyyy} - %p";
+        String s = "testing layout 1";
+        ma.setLayout(pattern);
+        ma.addLog(s);
+        assertEquals("testing layout 1 - main - 17 10 2024 - INFO", ma.getLogAsString(0));
+        ma.discardLog(0);
+        pattern = "%p - %d{dd MMM yyyy} in %t - %m";
+        s = "testing layout 2";
+        ma.setLayout(pattern);
+        ma.addLog(s);
+        assertEquals("INFO - 17 Oct 2024 in main - testing layout 2", ma.getLogAsString(0));
+        ma.discardLog(0);
+    }
+
+
 
 }
