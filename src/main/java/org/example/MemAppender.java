1ffbee0c664de1d186460de1bc6e4241180fb239
Ben <bjmullinger@gmail.com>
2024-10-18 14:37:05 +1300

    final changes

--- src/main/java/org/example/MemAppender.java
+++ src/main/java/org/example/MemAppender.java
@@ -4,14 +4,10 @@ import org.apache.log4j.*;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 import org.apache.log4j.Logger;
-import org.apache.velocity.app.VelocityEngine;
-import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
